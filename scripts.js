4e5f9fa30be6560eb3837c4afb79b2634d832a4a
Felipe Goncam <felipemaifredo@gmail.com>
2022-10-18 00:05:19 -0300

    v

--- scripts.js
+++ scripts.js
@@ -1,13 +1,10 @@
 // MENU MOBILE
     const btnMobile = document.querySelector('btn.nav-btn');
     const navItem = document.querySelectorAll('a.item-nav');
-
     const Nav = document.querySelector('nav#nav');
-
     function toggleMenu() {
         Nav.classList.toggle('active');
     }
-
     for (let i = 0; i < navItem.length; i++) { 
         navItem[i].addEventListener('click', () => {
             Nav.classList.toggle('active');
@@ -20,21 +17,18 @@ class SlideStories {
         this.slide = document.querySelector(`[data-slide="${id}"]`);
         this.init();
     }
-
     activeSlide(index) {
         this.active = index;
         this.itens.forEach(item => {
             item.classList.remove('active');
         });
         this.itens[index].classList.add('active');
-
         this.thumbItens.forEach(item => {
             item.classList.remove('active');
         });
         this.thumbItens[index].classList.add('active');
         this.autoSlide();
     }
-
     prev() {
         if (this.active > 0) {
             this.activeSlide(this.active - 1);
@@ -42,7 +36,6 @@ class SlideStories {
             this.activeSlide(this.itens.length - 1);
         }
     }
-    
     next() {
         if (this.active < this.itens.length - 1) {
             this.activeSlide(this.active + 1);
@@ -50,24 +43,20 @@ class SlideStories {
             this.activeSlide(0);
         }
     }
-
     addNavigation() {
         const nextBtn = this.slide.querySelector('.slide-next');
         const prevBtn = this.slide.querySelector('.slide-prev');
         nextBtn.addEventListener('click', this.next);
         prevBtn.addEventListener('click', this.prev);
     }
-
     addThunbItens() {
         this.itens.forEach(() => (this.thumb.innerHTML += '<span></span>'));
         this.thumbItens = Array.from(this.thumb.children);
     }
-
     autoSlide() {
         clearTimeout(this.timeOut);
         this.timeOut = setTimeout(this.next, 10000);
     }
-
     init() {
         this.itens = this.slide.querySelectorAll('.slide-itens > *');
         this.thumb = this.slide.querySelector('.slide-thumb');
@@ -77,8 +66,7 @@ class SlideStories {
         this.next = this.next.bind(this);
         this.prev = this.prev.bind(this);
         this.addNavigation();
-        this.autoSlide()
-        
+        this.autoSlide() 
     }  
 }
 new SlideStories('slide')
