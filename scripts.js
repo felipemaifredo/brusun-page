3bbba4552b6919bcd3b61af94bffdde5749e79f0
Felipe Maifredo <felipemaifredo@gmail.com>
2022-12-01 21:16:33 -0300

    v att

--- scripts.js
+++ scripts.js
@@ -153,3 +153,21 @@ inputRange.oninput = ( ()=>  {
    textResult.innerHTML = `R$ ${resultV},00 <span>/mês</span>`;
  })
 //
+//
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+//
